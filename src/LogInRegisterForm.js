accc89391436c435f90f83962297f88f51ba7650
Matt Keane <mattpkeane@gmail.com>
2020-05-02 20:03:02 -0500

    created log in/register form component

---
+++ src/LogInRegisterForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+import "materialize-css"
+
+export default class LogInRegisterForm extends Component {
+
+	render() {
+		return (
+			<React.Fragment>
+				Log In/Register Form
+			</React.Fragment>)
+	}
+}
