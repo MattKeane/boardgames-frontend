ee12fd8fbb72b140279035dcbb958470aee0f910
Matt Keane <mattpkeane@gmail.com>
2020-05-04 15:06:16 -0500

    New game modal displays drop down with list of existing genres

--- src/NewGameModal.js
+++ src/NewGameModal.js
@@ -1,7 +1,38 @@
 import React, { Component } from "react"
-import { Modal, Header } from "semantic-ui-react"
+import { Modal, Header, Form, Dropdown } from "semantic-ui-react"
 
 export default class NewGameModal extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			genreOptions: []
+		}
+	}
+
+	getGenres = async () => {
+		const url = process.env.REACT_APP_API_URL + "/api/v1/genres/"
+		const genreResponse = await fetch(url, {
+			credentials: "include"
+		})
+		const genreJson = await genreResponse.json()
+		const genres = []
+		for (let i = 0; i < genreJson.data.length; i++) {
+			const name = genreJson.data[i].name
+			genres.push({
+				key: name,
+				text: name,
+				value: name
+			})
+		}
+		this.setState({
+			genreOptions: genres
+		})
+	}
+
+	componentDidMount() {
+		this.getGenres()
+	}
 
 	render() {
 		return (
@@ -9,6 +40,11 @@ export default class NewGameModal extends Component {
 				<Header>
 					<h3>Add a Game</h3>
 				</Header>
+				<Form>
+					<Dropdown
+						options={this.state.genreOptions}
+					/>
+				</Form>
 			</Modal>
 		)
 	}
