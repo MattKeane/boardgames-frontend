9abc1f262bbbfd81b5801383cf60b72e3db2dee0
Matt Keane <mattpkeane@gmail.com>
2020-05-04 11:50:29 -0500

    Games now show up on cards

--- src/ListGames.js
+++ src/ListGames.js
@@ -1,11 +1,22 @@
 import React from "react"
+import { Card } from "semantic-ui-react"
 
 export default function ListGames(props) {
 	const games = props.games.map(game => {
 		return(
-			<div key={game.id}>
-				{game.title}
-			</div>
+			<Card key={game.id}>
+				<Card.Content>
+					<Card.Header>
+						{game.title}
+					</Card.Header>
+					<Card.Meta>
+						{game.publisher.username}
+					</Card.Meta>
+					<Card.Description>
+						{game.min_players}-{game.max_players} players
+					</Card.Description>
+				</Card.Content>
+			</Card>
 		)
 	})
 
