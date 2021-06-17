24632d39f70ea16b3b162207b7e948674aea54e7
Matt Keane <mattpkeane@gmail.com>
2021-06-17 10:19:52 -0500

    refactored log in component as a function

--- src/LogInForm.js
+++ src/LogInForm.js
@@ -1,47 +1,45 @@
-import React, { Component } from "react"
+import React from "react"
 import { Form, Input, Button } from "semantic-ui-react"
 
 
-export default class LogInForm extends Component {
+export default function LogInForm(props) {
 
-	render() {
-		return (
-			<React.Fragment>
-				<div className="login-form">
-					<header>
-						<h2>Sign In</h2>
-					</header>
-					<div>{this.props.message}</div>
-					<Form onSubmit={this.props.logIn}>
-						<Form.Field>
-							<Input
-								inverted								
-								label="Email"
-								placeholder="Enter Email"
-								name="email"
-								value={this.props.email}
-								onChange={this.props.handleChange}
-							/>
-						</Form.Field>
-						<Form.Field>
-							<Input
-								inverted
-								type="password"
-								label="Password"
-								name="password"
-								value={this.props.password}
-								onChange={this.props.handleChange}
-							/>
-						</Form.Field>
-						<div>
-							<Button color="green">
-								Sign In							
-							</Button>
-						</div>
-					</Form>
-					<p onClick={this.props.toggleRegister}>Need an account? Sign up!</p>
-				</div>
-			</React.Fragment>
-		)
-	}
+	return (
+		<React.Fragment>
+			<div className="login-form">
+				<header>
+					<h2>Sign In</h2>
+				</header>
+				<div>{ props.message }</div>
+				<Form onSubmit={ props.logIn }>
+					<Form.Field>
+						<Input
+							inverted								
+							label="Email"
+							placeholder="Enter Email"
+							name="email"
+							value={ props.email }
+							onChange={ props.handleChange }
+						/>
+					</Form.Field>
+					<Form.Field>
+						<Input
+							inverted
+							type="password"
+							label="Password"
+							name="password"
+							value={ props.password }
+							onChange={ props.handleChange }
+						/>
+					</Form.Field>
+					<div>
+						<Button color="green">
+							Sign In							
+						</Button>
+					</div>
+				</Form>
+				<p onClick={ props.toggleRegister }>Need an account? Sign up!</p>
+			</div>
+		</React.Fragment>
+	)
 }
