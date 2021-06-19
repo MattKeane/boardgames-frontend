f39c3d0c4458026fb1bd75e1d069470d7dd2fd79
Matt Keane <mattpkeane@gmail.com>
2021-06-19 12:56:59 -0500

    refactored NewGameModal to be a function component with hooks

--- src/NewGameModal.js
+++ src/NewGameModal.js
@@ -1,117 +1,112 @@
-import React, { Component } from "react"
-import { Modal, Header, Form, Dropdown, Input, Button } from "semantic-ui-react"
+import React, { useState, useEffect } from "react";
+import { Modal, Header, Form, Dropdown, Input, Button } from "semantic-ui-react";
 
-export default class NewGameModal extends Component {
-	constructor(props) {
-		super(props)
+export default function NewGameModal(props) {
 
-		this.state = {
-			genreOptions: [],
-			genres: [],
-			title: "",
-			min_players: null,
-			max_players: null
-		}
+	const [genreOptions, setGenreOptions] = useState([]);
+	const [genres, setGenres] = useState([]);
+	const [title, setTitle] = useState('');
+	const [minPlayers, setMinPlayers] = useState(0);
+	const [maxPlayers, setMaxPlayers] = useState(0);
+
+	useEffect(() => {
+		const getGenres = async () => {
+			const url = process.env.REACT_APP_API_URL + "/api/v1/genres/";
+			const genreResponse = await fetch(url, {
+				credentials: "include"
+			});
+			const genreJson = await genreResponse.json();
+			const fetchedGenres = [];
+			for (let i = 0; i < genreJson.data.length; i++) {
+				const name = genreJson.data[i].name;
+				fetchedGenres.push({
+					key: name,
+					text: name,
+					value: name
+				});
+			}
+			setGenreOptions(fetchedGenres);
+		};
+		getGenres();	
+	}, []);
+
+	const handleChange = (e, { value }) => {
+		const setStateObj = {
+			genres: setGenres,
+			title: setTitle,
+			minPlayers: setMinPlayers,
+			maxPlayers: setMaxPlayers,
+		};
+		setStateObj[e.target.name](value);
 	}
 
-	getGenres = async () => {
-		const url = process.env.REACT_APP_API_URL + "/api/v1/genres/"
-		const genreResponse = await fetch(url, {
-			credentials: "include"
-		})
-		const genreJson = await genreResponse.json()
-		const genres = []
-		for (let i = 0; i < genreJson.data.length; i++) {
-			const name = genreJson.data[i].name
-			genres.push({
-				key: name,
-				text: name,
-				value: name
-			})
-		}
-		this.setState({
-			genreOptions: genres
-		})
-	}
-
-	componentDidMount() {
-		this.getGenres()
-	}
-
-	handleChange = (e, { value }) => {
-		this.setState({
-			[e.target.name]: value
-		})
-	}
-
-	handleGenreChange = (e, { value }) => {
-		this.setState({
-			genres: value
-		})
+	const handleGenreChange = (e, { value }) => {
+		setGenres(value);
 	}
 	
 
-	handleAddition = (e, { value }) => {
-		this.setState({
-			genreOptions: [{text: value, value }, ...this.state.genreOptions]
+	const handleAddition = (e, { value }) => {
+		setGenreOptions([{ text: value, value }, ...genreOptions])
+	}
+
+	const handleSubmit = () => {
+		props.addGame({
+			min_players: minPlayers,
+			max_players: maxPlayers,
+			title,
+			genres,
 		})
 	}
 
-	handleSubmit = () => {
-		this.props.addGame(this.state)
-	}
-
-	render() {
-		return (
-			<Modal open={true} closeIcon={true} onClose={this.props.closeModal}>
-				<Header>
-					<h3>Add a Game</h3>
-				</Header>
-				<Form onSubmit={this.handleSubmit}>
-					<Form.Field>
-						<Input
-							label="Game Title"
-							type="text"
-							name="title"
-							value={this.state.title}
-							placeholder="Enter Game Title"
-							onChange={this.handleChange}
-						/>
-					</Form.Field>
-					<Form.Field>
-						<Input
-							label="Minimum Number of Players"
-							type="number"
-							name="min_players"
-							value={this.state.min_players}
-							onChange={this.handleChange}
-						/>
-					</Form.Field>
-					<Form.Field>
-						<Input
-							label="Maximum Number of Players"
-							type="number"
-							name="max_players"
-							value={this.state.max_players}
-							onChange={this.handleChange}
-						/>
-					</Form.Field>
-					<Form.Field>
-						<Dropdown
-							name="genres"
-							options={this.state.genreOptions}
-							placeholder="Choose Genres"
-							search
-							selection
-							multiple
-							allowAdditions
-							onChange={this.handleGenreChange}
-							onAddItem={this.handleAddition}
-						/>
-					</Form.Field>
-					<Button>Add Game</Button>
-				</Form>
-			</Modal>
-		)
-	}
-}
+	return (
+		<Modal open={ true } closeIcon={ true } onClose={ props.closeModal }>
+			<Header>
+				<h3>Add a Game</h3>
+			</Header>
+			<Form onSubmit={ handleSubmit }>
+				<Form.Field>
+					<Input
+						label="Game Title"
+						type="text"
+						name="title"
+						value={ title }
+						placeholder="Enter Game Title"
+						onChange={ handleChange }
+					/>
+				</Form.Field>
+				<Form.Field>
+					<Input
+						label="Minimum Number of Players"
+						type="number"
+						name="minPlayers"
+						value={ minPlayers }
+						onChange={ handleChange }
+					/>
+				</Form.Field>
+				<Form.Field>
+					<Input
+						label="Maximum Number of Players"
+						type="number"
+						name="maxPlayers"
+						value={ maxPlayers }
+						onChange={ handleChange }
+					/>
+				</Form.Field>
+				<Form.Field>
+					<Dropdown
+						name="genres"
+						options={ genreOptions }
+						placeholder="Choose Genres"
+						search
+						selection
+						multiple
+						allowAdditions
+						onChange={ handleGenreChange }
+						onAddItem={ handleAddition }
+					/>
+				</Form.Field>
+				<Button>Add Game</Button>
+			</Form>
+		</Modal>
+	)
+};
